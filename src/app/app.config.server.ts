6e74d05b60357a3c9a808da222fd225e8f1af69b
Jali <fondajalil@gmail.com>
2025-09-17 13:51:37 +0200

    add ssr again

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,18 +1,11 @@
-// src/app/app.config.server.ts
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
+//import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 
-const serverRoutes = [
-  { path: 'ssr-route', renderMode: 'server' },
-  { path: 'prerender-route', renderMode: 'prerender' },
-  { path: '**', renderMode: 'client' }, // Default to client-side rendering
-];
-
-const serverAppConfig: ApplicationConfig = {
+const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),
-  ],
+    //provideServerRendering()
+  ]
 };
 
-export const config = mergeApplicationConfig(appConfig, serverAppConfig);
+export const config = mergeApplicationConfig(appConfig, serverConfig);
